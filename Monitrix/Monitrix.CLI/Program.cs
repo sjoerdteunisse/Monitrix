5f6e23e5cd45ff470bd986decddc085fa7993aa7
Sjoerd Teunisse <sjoerdteunisse@gmail.com>
2021-07-12 22:52:31 +0200

    Base SLN and core handler

---
+++ Monitrix/Monitrix.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Monitrix.Core;
+using System;
+
+namespace Monitrix.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var excH = new ExceptionHandlerRegistration(ExceptionHandlerRegistration.HandlingType.SILENT);
+            try
+            {
+                throw new SystemException(":");
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Monitrix/Monitrix.Core/ExceptionHandlerRegistration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace Monitrix.Core
+{
+    public class ExceptionHandlerRegistration
+    {
+       public enum HandlingType { SILENT }
+
+        private HandlingType _handlingType;
+
+        public ExceptionHandlerRegistration(HandlingType handlingType) 
+        {
+            this.Setup();
+            this._handlingType = handlingType;
+        }
+
+        private void Setup() 
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (sender != null)
+                Console.WriteLine(sender.ToString());
+
+            Console.WriteLine("An unhandeld exception occured");
+
+            Console.WriteLine(e.ExceptionObject.ToString());
+        }
+
+        static void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+        {
+            if (sender != null)
+                Console.WriteLine(sender.ToString());
+
+            Console.WriteLine("An exception occured but was catched");
+            
+            Console.WriteLine(e.Exception.ToString());
+        }
+    }
+}
